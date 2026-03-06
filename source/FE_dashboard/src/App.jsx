b10671f20b6c57472288dae76a7d36ff4c15e06b
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2026-03-06 18:38:13 +0100

    bozza dashboard

--- source/FE_dashboard/src/App.jsx
+++ source/FE_dashboard/src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import MarsDashboard from './components/MarsDashboard';
+
 function App() {
   return (
-    <div className="bg-red-500 p-10">
-      <h1 className="text-white text-4xl font-bold">Tailwind è attivo! 🚀</h1>
+    <div>
+      <MarsDashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
