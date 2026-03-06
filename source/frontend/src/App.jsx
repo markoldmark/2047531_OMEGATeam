46f7460446840ae032447cb2493dd6d5e5a5256b
Marco Marcovecchio <marcoldmark@gmail.com>
2026-03-06 16:22:02 +0100

    frontend implementation (no container)

---
+++ source/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-500 p-10">
+      <h1 className="text-white text-4xl font-bold">Tailwind è attivo! 🚀</h1>
+    </div>
+  )
+}
+
+export default App
